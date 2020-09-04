67df82de21785eeba681f87c3d8fbd163984c612
espenthaem <e.tierolff@gmail.com>
2020-09-04 11:49:50 +0200

    news page design changes

--- template-news.php
+++ template-news.php
@@ -33,13 +33,13 @@ Template Name: News
 							<p><?php the_title();?></p>
 						</div>
 
-						<div>
-		            		<img src="<?php the_post_thumbnail_url('full');?>">
-		            	</div>	
-
 						<div class="single-text-div">
 							<p><?php the_content();?></p> 
 						</div>
+
+						<div>
+		            		<img src="<?php the_post_thumbnail_url('full');?>">
+		            	</div>	
 
 						<div class="image-padding">
                         </div>
