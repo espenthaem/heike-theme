62871510163568bc4a83641f1b5f701fcc5ca118
espenthaem <e.tierolff@gmail.com>
2020-09-04 10:18:26 +0200

    implemented archive.php

--- archive.php
+++ archive.php
@@ -1,29 +1,33 @@
 <?php get_header();?>
 
+
+<?php get_template_part('includes/section','sidebarnav');?>
+
+<div id="content-section">
+
 	<?php if(have_posts() ): while(have_posts() ): the_post();?>
-
-			<div class="card mb-3">
 				
-				<div class="justify-content-center align-items-center"> 
+			<div class="justify-content-center align-items-center"> 
 
 
-					<?php if(has_post_thumbnail()):?>
+				<?php if(has_post_thumbnail()):?>
 
-						<img src="<?php the_post_thumbnail_url('thumbnail');?>" alt="<?php the_title();?>" width=full>
+					<img src="<?php the_post_thumbnail_url('thumbnail');?>" alt="<?php the_title();?>" width=full>
 
-					<?php endif;?>
+				<?php endif;?>
 
-					<div class="blog-content">				
-						<h3><?php the_title();?></h3>
+				<div class="blog-content">				
+					<h3><?php the_title();?></h3>
 
-						<?php the_excerpt();?>
+					<?php the_excerpt();?>
 
-						<a href="<?php the_permalink();?>">Read More </a>
-					</div>
+					<a href="<?php the_permalink();?>">Read More </a>
 				</div>
 			</div>
 
 	<?php endwhile; else: endif;?>
+
+</div>
 
 
 <?php get_footer();?>
