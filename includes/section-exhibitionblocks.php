180e741bd9faf41138c66a51c18afdcebf6f4d62
espenthaem <e.tierolff@gmail.com>
2020-09-09 11:27:15 +0200

    implemented dynamic title-div sizing for exh blocks and more styling adjustments

--- includes/section-exhibitionblocks.php
+++ includes/section-exhibitionblocks.php
@@ -15,10 +15,12 @@ if ($the_query->have_posts()) {
 		$the_query->the_post(); 
 		/* Open flex row div if first element */
 		if ($counter % 2 == 0) { 
-			echo '  <div class="views-row-div">';
+			?>
+			<div class="views-row-div" id="row_<?php echo $counter / 2;?>">
+			<?php
 		} ?>
 		<!-- Fill exhibition block with the appropriate content -->
-		<div class="views-single-div">
+		<div class="views-single-div"> 
 			
 			<div class="single-title-div">
 				<p><?php the_title();?></p>
@@ -27,7 +29,7 @@ if ($the_query->have_posts()) {
 			<div style="padding-top: 20px;">
 			</div>
 
-			<div class="single-video-div">
+			<div class="single-video-div" style="display:block;">
 				<iframe src="<?php echo get_post_meta(get_the_ID(), 'vimeo-link', true);?>?title=0&byline=0&portrait=0"> </iframe>
 			</div>
 
@@ -47,11 +49,31 @@ if ($the_query->have_posts()) {
 		<?php
 		/* Close flex row div after second element */
 		if ($counter % 2 == 1) { 
-			echo '  </div>';
-		} 
+			?>
+			</div>
+
+			<!-- Force same title-div-height within the row using js */ -->
+			<script>
+				var row = document.getElementById('row_' + '<?php echo ($counter - 1) / 2;?>');
+				var title_divs = row.getElementsByClassName('single-title-div');
+
+				var max_div_height = Math.max(title_divs[0].clientHeight, title_divs[1].clientHeight);
+				
+				for (i=0; i<title_divs.length; ++i) {
+					title_divs[i].setAttribute("style", "height:" + max_div_height + "px")
+				}
+				
+			</script>
+
+
+		<?php
+		}?>
+		
+		<?php
 		/* Update counter */
 		++$counter;
-    }      
-	} ?>
+    } 
+
+} ?>
 
 <?php wp_reset_postdata(); ?>
