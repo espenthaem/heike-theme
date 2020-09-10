338ee1d0b01927eddf1b635b5bce723deb6177ac
espenthaem <e.tierolff@gmail.com>
2020-09-10 22:02:07 +0200

    news page redesign

--- includes/section-newsblocks.php
+++ includes/section-newsblocks.php
@@ -1,18 +1,20 @@
-<div>
+<div class="single-news-container">
 
-	<div class="single-title-div">
-		<p><?php the_title();?></p>
+
+		<div class="single-image-div-news">
+			<img src="<?php the_post_thumbnail_url('full');?>">
+		</div>
+
+	<div style="width: 75%; padding-left: 50px;">
+
+		<div class="single-title-div">
+			<p><?php the_title();?></p>
+		</div>
+
+		<div class="single-text-div single-text-div-news">
+			<p><?php the_content();?></p> 
+		</div>
+
 	</div>
-
-	<div class="single-text-div single-text-div-news">
-		<p><?php the_content();?></p> 
-	</div>
-
-	<div class="single-image-div-news">
-		<img src="<?php the_post_thumbnail_url('full');?>">
-	</div>	
-
-	<div class="image-padding">
-    </div>
 
 </div>
